b132a1440a9d3337a7b25e71ed16e42b3cc39f93
Jim Graham <jim.graham@shopify.com>
2024-04-24 16:39:41 -0400

    Remove finding the item ID
    Replaced by a separate action.
    https://github.com/jimgraham/find-in-projectV2

--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,14 @@
 import * as core from "@actions/core";
-import * as github from "@actions/github";
 import { graphql } from "@octokit/graphql";
 
 import { run } from "./run";
 
 async function main(): Promise<void> {
-  const issueNumber = Number(core.getInput("issue-number"));
-  if (Number.isNaN(issueNumber)) {
-    throw new Error("issue-number must be a number");
-  }
-  const currentRepository = github.context.payload.repository;
-  const projectOwner = core.getInput("project-owner") || currentRepository?.owner.login;
-  if (!projectOwner) {
-    throw new Error("project-owner must be specified, unable to determine from context");
-  }
-
-  const issueOwner = core.getInput("issue-owner") || currentRepository?.owner.login;
-  const issueRepository = core.getInput("issue-repository") || currentRepository?.name;
-  if (!issueOwner || !issueRepository) {
-    throw new Error("issue-owner and issue-repository must be set, unable to determine from context");
-  }
   const token = core.getInput("token", { required: true });
 
   await run({
-    issueNumber: issueNumber,
-    issueOwner: issueOwner,
-    issueRepository: issueRepository,
+    itemId: Number(core.getInput("item-id", { required: true })),
     projectNumber: Number(core.getInput("project-number", { required: true })),
-    projectOwner: projectOwner,
-    octokit: github.getOctokit(token),
     graphqlWithAuth: graphql.defaults({
       headers: {
         authorization: `token ${token}`
